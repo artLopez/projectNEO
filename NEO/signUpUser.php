5b3aa5ab944d4277179b60c9bac69947817b467d
Hector Sandoval <hesandova@gmail.com>
2015-05-06 13:10:59 -0700

    Add references to signUpUser

--- NEO/signUpUser.php
+++ NEO/signUpUser.php
@@ -5,3 +5,18 @@
  * Date: 5/3/15
  * Time: 4:57 PM
  */
+
+$userSql = "INSERT INTO `users`(`username`, `password`, `email`,
+                `first_name`, `last_name`, `phone_number`)
+                VALUES (:username, :password,:email, :first_name, :last_name,:phone_number)";
+
+$userNamedParameters = array();
+$userNamedParameters[":username"] = $_POST['username'];
+$userNamedParameters[":password"]  = hash("sha256",$_POST['password']);
+$userNamedParameters[":email"] = $_POST["email"];
+$userNamedParameters[":first_name"]       = $_POST['givenName'];
+$userNamedParameters[":last_name"]    = $_POST['surname'];
+$userNamedParameters["phone_number"] = $_POST['phone_number'];
+
+$stmt2 = $dbConn->prepare($userSql);
+$stmt2->execute($userNamedParameters);
